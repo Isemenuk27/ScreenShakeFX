dd9b323c3307d3b5d31c2a33e7596f9c2781164d
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2022-11-13 15:40:26 +0200

    Add files via upload

--- Screen Shake/lua/autorun/screenshake.lua
+++ Screen Shake/lua/autorun/screenshake.lua
@@ -1,100 +1,57 @@
 if CLIENT then
-CreateClientConVar("scrsk_enable_cl", "1", true, false)
-CreateClientConVar("scrsk_amplitude", "3", true, false)
-CreateClientConVar("scrsk_frequency", "3", true, false)
-CreateClientConVar("scrsk_duration", "2", true, false)
+	local scrsk_enable_cl = CreateClientConVar("scrsk_enable_cl", "1", true, false)
+	local scrsk_amplitude = CreateClientConVar("scrsk_amplitude", "3", true, false)
+	local scrsk_frequency = CreateClientConVar("scrsk_frequency", "3", true, false)
+	local scrsk_duration = CreateClientConVar("scrsk_duration", "2", true, false)
 
-/*
-CreateClientConVar("scrsk_enable_pp", "1", true, false)
-CreateClientConVar("scrsk_amplitude_pp", "5", true, false)
-CreateClientConVar("scrsk_frequency_pp", "3", true, false)
+	local Enabled = scrsk_enable_cl:GetBool()
+	local Amplitude = scrsk_amplitude:GetFloat()
+	local Frequency = scrsk_frequency:GetFloat()
+	local Duration = scrsk_duration:GetFloat()
 
-local EnabledPP = false
-local NextShake = CurTime()
+	cvars.AddChangeCallback("scrsk_enable_cl", function(CVarName, OldVar, NewVar) Enabled = tobool(NewVar) end)
+	cvars.AddChangeCallback("scrsk_amplitude", function(CVarName, OldVar, NewVar) Amplitude = tonumber(NewVar) end)
+	cvars.AddChangeCallback("scrsk_frequency", function(CVarName, OldVar, NewVar) Frequency = tonumber(NewVar) end)
+	cvars.AddChangeCallback("scrsk_duration", function(CVarName, OldVar, NewVar) Duration = tonumber(NewVar) end)
 
-cvars.AddChangeCallback("scrsk_enable_pp", function(convar_name, value_old, value_new) EnabledPP = tobool(value_new) end)
-*/
+	net.Receive( "DoExplosionShake", function(len)
+		if !Enabled then return end
+		local dmg = net.ReadInt(14)
+		util.ScreenShake( Vector(0, 0, 0), Amplitude * ( ply:GetMaxHealth() / dmg ), Frequency, Duration, 0 )
+	end )
+	
+	hook.Add( "AddToolMenuCategories", "postprocessingMenuShake", function()
+		spawnmenu.AddToolCategory( "Options", "postprocessing", "#spawnmenu.category.postprocess" )
+	end )
 
-net.Receive( "DoExplosionShake", function(len)
-	if (!GetConVar("scrsk_enable_cl"):GetBool() || EnabledPP) then return end
-	local dmg = net.ReadInt(14)
-	local ply = net.ReadEntity()
-	if !IsValid( ply ) then return end
-	if LocalPlayer() != ply then return end
+	hook.Add( "PopulateToolMenu", "ScreenShakeMenu", function()
+		spawnmenu.AddToolMenuOption( "Options", "postprocessing", "postprocessingMenuShake", "#Screen Shake", "", "", function( panel )
+			panel:ClearControls()
 
-	util.ScreenShake( Vector(0, 0, 0), GetConVar("scrsk_amplitude"):GetFloat() * ( dmg / ply:GetMaxHealth() ), GetConVar("scrsk_frequency"):GetFloat(), GetConVar("scrsk_duration"):GetFloat(), 0 )
-end )
-/*
-hook.Add( "Think", "ScreenShakeHook", function()
-	if !EnabledPP then return end
-	if ( NextShake > CurTime() && NextShake == 0 ) then return end
-
-	util.ScreenShake( Vector(0, 0, 0), GetConVar("scrsk_amplitude_pp"):GetFloat(), GetConVar("scrsk_frequency_pp"):GetFloat(), 2, 0 )
-	NextShake = CurTime() + 2
-end )
-*/
+			if LocalPlayer():IsAdmin() then
+				panel:CheckBox( "Enable on all clients", "scrsk_enable" )
+			end
+			panel:CheckBox( "Enable", "scrsk_enable_cl" )
+			panel:NumSlider( "Amplitude", "scrsk_amplitude", 0.1, 7 )
+			panel:NumSlider( "Frequency", "scrsk_frequency", 0.01, 5 )
+			panel:NumSlider( "Duration", "scrsk_duration", 0.5, 5 )
+		end )
+	end )
 end
 
 if SERVER then
-
 	util.AddNetworkString( "DoExplosionShake" )
 
-	CreateConVar("scrsk_enable", "1", { FCVAR_ARCHIVE }, "Enable screen shake after explosion" )
+	local Enabled = CreateConVar("scrsk_enable", "1", { FCVAR_ARCHIVE }, "Enable screen shake after explosion" )
 
 	hook.Add( "EntityTakeDamage", "ExplosionShakeHook", function( target, dmginfo )
 
-		if !GetConVar("scrsk_enable"):GetBool() then return end
+		if !Enabled:GetBool() then return end
 		if !target:IsPlayer() then return end
 		if !dmginfo:IsDamageType(DMG_BLAST) then return end
 
 		net.Start( "DoExplosionShake" )
 			net.WriteInt( dmginfo:GetDamage(), 14 )
-			net.WriteEntity( target )
-		net.Broadcast()
+		net.Send(target)
 	end)
 end
-
-hook.Add( "AddToolMenuCategories", "postprocessingMenuShake", function()
-	spawnmenu.AddToolCategory( "Options", "postprocessing", "#spawnmenu.category.postprocess" )
-end )
-
-hook.Add( "PopulateToolMenu", "ScreenShakeMenu", function()
-	spawnmenu.AddToolMenuOption( "Options", "postprocessing", "postprocessingMenuShake", "#Screen Shake", "", "", function( panel )
-		panel:ClearControls()
-
-		if LocalPlayer():IsAdmin() then
-			panel:CheckBox( "Enable on all clients", "scrsk_enable" )
-		end
-		panel:CheckBox( "Enable", "scrsk_enable_cl" )
-		panel:NumSlider( "Amplitude", "scrsk_amplitude", 0.1, 7 )
-		panel:NumSlider( "Frequency", "scrsk_frequency", 0.01, 5 )
-		panel:NumSlider( "Duration", "scrsk_duration", 0.5, 5 )
-	end )
-end )
-/*
-list.Set( "PostProcess", "#Screen Shake", {
-
-	icon = "gui/postprocess/accummotionblur.png",
-	convar = "scrsk_enable_pp",
-	category = "#effects_pp",
-
-	cpanel = function( CPanel )
-
-		//CPanel:AddControl( "Header", { Description = "#motion_blur_pp.desc" } )
-
-		CPanel:AddControl( "CheckBox", { Label = "Enable", Command = "scrsk_enable_pp" } )
-
-		local params = { Options = {}, CVars = {}, MenuButton = "1", Folder = "screenshake" }
-		params.Options[ "#preset.default" ] = { scrsk_enable_pp = "0", scrsk_amplitude_pp = "5", scrsk_frequency_pp = "3" }
-		params.CVars = table.GetKeys( params.Options[ "#preset.default" ] )
-		CPanel:AddControl( "ComboBox", params )
-
-		CPanel:AddControl( "Slider", { Label = "Amplitude", Command = "scrsk_amplitude_pp", Type = "Float", Min = "0", Max = "10" } )
-		CPanel:AddControl( "Slider", { Label = "Frequency", Command = "scrsk_frequency_pp", Type = "Float", Min = "0", Max = "10" } )
-
-		//CPanel:AddControl( "Slider", { Label = "#motion_blur_pp.delay", Command = "", Type = "Float", Min = "0", Max = "1" } )
-
-	end
-
-} )
-*/
